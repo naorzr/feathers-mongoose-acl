8ec21d5c45579ed9ad0120cba4d00f8ac2b3ee1b
naor <naorzruk@gmail.com>
2019-09-01 22:02:45 +0300

    add first test for AND method

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,13 @@
-'use strict';
-const expect = require('chai').expect;
+import {expect} from 'chai'
+import {AND} from "../lib";
+import {HookContext} from "@feathersjs/feathers";
 
-
-describe('getPlural function test 22', () => {
-    it('should return hello', () => {
-        expect(true).to.equals(true)
+describe('Declarative AND method testing', () => {
+    it('should return a valid criteria object', () => {
+        const fakeContext = {} as HookContext
+        const condition1 = () => ({field: 'value1'})
+        const condition2 = () => ({field2: 'value2'})
+        const res = AND(condition1, condition2)(fakeContext)
+        expect(res).to.deep.equals({$and: [condition1(),condition2()]})
     });
 });
