a1a3ec16621e4453b1dda955d6b021c0f7320398
naor <naorzruk@gmail.com>
2019-09-01 21:51:31 +0300

    sample test setup

---
+++ test/test.ts
@@ -0,0 +1,9 @@
+'use strict';
+const expect = require('chai').expect;
+
+
+describe('getPlural function test 22', () => {
+    it('should return hello', () => {
+        expect(true).to.equals(true)
+    });
+});
